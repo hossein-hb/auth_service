b60dae6855a93fe97baf0c638496cff991c7d2ea
hossein_hb <h.habibi.401@proton.me>
2026-08-28 14:18:50 -0400

    initialize auth_service project.

---
+++ src/main/java/com/traazu/auth_service/AuthServiceApplication.java
@@ -0,0 +1,19 @@
+package com.traazu.auth_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
+@SpringBootApplication
+public class AuthServiceApplication {
+
+	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+        dotenv.entries().forEach(entry -> 
+            System.setProperty(entry.getKey(), entry.getValue())
+        );
+		SpringApplication.run(AuthServiceApplication.class, args);
+	}
+
+}
