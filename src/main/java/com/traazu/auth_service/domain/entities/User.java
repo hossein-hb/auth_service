275f906154be4ab22e95b0a653fd3da48f9e6b0b
hossein_hb <h.habibi.401@proton.me>
2026-08-29 04:50:14 -0400

    add firstName and lastName fields to User.java

--- src/main/java/com/traazu/auth_service/domain/entities/User.java
+++ src/main/java/com/traazu/auth_service/domain/entities/User.java
@@ -34,6 +34,16 @@ public class User {
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
+    @NotBlank(message = "First name is required!")
+    @Column(name = "first_name", nullable = false)
+    @Size(max = 20, message = "First name must be less than 20 characters!")
+    private String firstName;
+
+    @NotBlank(message = "Last name is required!")
+    @Column(name = "last_name", nullable = false)
+    @Size(max = 20, message = "last name must be less than 20 characters!")
+    private String lastName;
+
     @Email(message = "Email format is invalid!")
     @NotBlank(message = "Email is required!")
     @Column(name = "email", nullable = false, updatable = false, unique = true)
