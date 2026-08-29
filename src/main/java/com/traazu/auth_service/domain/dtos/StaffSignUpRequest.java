739760ea20c68848fa3ddffd68ee7b245585bc26
hossein_hb <h.habibi.401@proton.me>
2026-08-29 05:43:23 -0400

    create StaffSignUpRequest.java dto.

---
+++ src/main/java/com/traazu/auth_service/domain/dtos/StaffSignUpRequest.java
@@ -0,0 +1,17 @@
+package com.traazu.auth_service.domain.dtos;
+
+import java.util.UUID;
+
+import com.traazu.auth_service.domain.enums.UserRole;
+
+public record StaffSignUpRequest(
+    String firstName,
+    String lastName,
+    String email,
+    String password,
+    String repeatPassword,
+    UserRole role,
+    UUID createdBy,
+    String createdByName,
+    UserRole createdByRole
+) {}
