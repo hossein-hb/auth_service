645926526039e94e3b40a6d7296c641e45bccbf2
hossein_hb <h.habibi.401@proton.me>
2026-08-29 05:34:19 -0400

    add Getter and Setter annotations.

--- src/main/java/com/traazu/auth_service/domain/entities/User.java
+++ src/main/java/com/traazu/auth_service/domain/entities/User.java
@@ -22,12 +22,16 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
 @Table(name = "users")
+@Getter
+@Setter
 public class User {
 
     @Id
