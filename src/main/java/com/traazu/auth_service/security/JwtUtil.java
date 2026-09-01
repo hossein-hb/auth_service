04c49a2bb1e6278eb77237081f1e7c3554cf8704
hossein_hb <h.habibi.401@proton.me>
2026-09-01 11:53:59 -0400

    create JwtUtil.java

---
+++ src/main/java/com/traazu/auth_service/security/JwtUtil.java
@@ -0,0 +1,81 @@
+package com.traazu.auth_service.security;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+import javax.crypto.SecretKey;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.traazu.auth_service.domain.enums.UserRole;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+
+public class JwtUtil {
+
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    @Value("${jwt.expiration")
+    private long jwtExpiration;
+
+    public String extractUsername(String token) {
+        return extractClaim(token, claims -> claims.getSubject());
+    }
+
+    public String extractRole(String token) {
+        return extractClaim(token, claims -> claims.get("role", String.class));
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        Claims allClaims = extractAllClaims(token);
+        return claimsResolver.apply(allClaims);
+    }
+
+    public String generateToken(UserDetails userDetails, UserRole role) {
+        Map<String, Object> extraClaims = new HashMap<>();
+        extraClaims.put("role", role);
+        return buildToken(userDetails, extraClaims, jwtExpiration);
+    }
+
+    public String buildToken(UserDetails userDetails, Map<String, Object> extraClaims, long expiration) {
+        return Jwts.builder()
+                    .claims(extraClaims)
+                    .subject(userDetails.getUsername())
+                    .issuedAt(new Date(System.currentTimeMillis()))
+                    .expiration(new Date(System.currentTimeMillis() + expiration))
+                    .signWith(getSignInKey())
+                    .compact();
+    }
+
+    public boolean isTokenExpired(String token) {
+        return extractAllClaims(token).getExpiration().before(new Date());
+    }
+
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        String username = userDetails.getUsername();
+        Claims allClaims = extractAllClaims(token);
+        return allClaims.getSubject().equals(username)
+                & extractAllClaims(token).getExpiration().before(new Date());
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser()
+                    .verifyWith(getSignInKey())
+                    .build()
+                    .parseSignedClaims(token)
+                    .getPayload();
+    }
+
+    private SecretKey getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+    
+}
