58471f217e9076d504c14b9b126ede88389a204e
hossein_hb <h.habibi.401@proton.me>
2026-09-02 07:15:41 -0400

    create AuthResponse.java

---
+++ src/main/java/com/traazu/auth_service/domain/dtos/auth/AuthResponse.java
@@ -0,0 +1,14 @@
+package com.traazu.auth_service.domain.dtos.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class AuthResponse {
+
+    private String token;
+    
+}
