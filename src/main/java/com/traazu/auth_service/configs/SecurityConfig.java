162e9c58065a965f47219bf1d061eff0d558a196
hossein_hb <h.habibi.401@proton.me>
2026-09-07 08:21:58 -0400

    create SecurityConfig.java for passwordEncoder.

---
+++ src/main/java/com/traazu/auth_service/configs/SecurityConfig.java
@@ -0,0 +1,16 @@
+package com.traazu.auth_service.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    
+}
