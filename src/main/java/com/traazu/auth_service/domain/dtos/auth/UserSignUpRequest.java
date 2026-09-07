b9bb151f5bddf181fa751097a37a84eba0bf83c8
hossein_hb <h.habibi.401@proton.me>
2026-09-07 06:01:25 -0400

    move UserSignUpRequest.java to auth folder.

--- src/main/java/com/traazu/auth_service/domain/dtos/auth/UserSignUpRequest.java
+++ src/main/java/com/traazu/auth_service/domain/dtos/auth/UserSignUpRequest.java
@@ -1,6 +1,7 @@
-package com.traazu.auth_service.domain.dtos;
+package com.traazu.auth_service.domain.dtos.auth;
 
 public record UserSignUpRequest(
+    String token,
     String firstName,
     String lastName,
     String email,
