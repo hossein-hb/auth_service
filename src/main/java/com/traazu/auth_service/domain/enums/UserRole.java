918a8275de91738f66ee6dc74383ba37417edc33
hossein_hb <h.habibi.401@proton.me>
2026-09-17 14:16:51 +0330

    add isValid method to UserRole.java

--- src/main/java/com/traazu/auth_service/domain/enums/UserRole.java
+++ src/main/java/com/traazu/auth_service/domain/enums/UserRole.java
@@ -1,9 +1,16 @@
 package com.traazu.auth_service.domain.enums;
 
+import java.util.Arrays;
+
 public enum UserRole {
 
     SUPPORT,
     ADMIN,
-    USER
+    USER;
+
+    public static boolean isValid(String role) {
+        return role != null && Arrays.stream(values())
+                .anyMatch(r -> r.name().equals(role));
+    }
     
 }
