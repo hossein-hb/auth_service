ca473aefb89b67b2c28453089b0d38d604b2a0ec
hossein_hb <h.habibi.401@proton.me>
2026-09-18 16:36:13 +0330

    create LogInRedisPrefixes.java

---
+++ src/main/java/com/traazu/auth_service/services/auth/log_in/LogInRedisPrefixes.java
@@ -0,0 +1,9 @@
+package com.traazu.auth_service.services.auth.log_in;
+
+public class LogInRedisPrefixes {
+
+    public static final String IP_RATE_LIMIT = "ratelimit:login:ip:";
+
+    public static final String USERNAME_RATE_LIMIT = "ratelimit:login:username:";
+    
+}
