b253220cffaf22605b43f1d10213004aba0b1e05
hossein_hb <h.habibi.401@proton.me>
2026-09-18 16:34:46 +0330

    add @NotNull and @NotBlank annotations and add ipAddress field to LogInRequest.java

--- src/main/java/com/traazu/auth_service/domain/dtos/auth/LogInRequest.java
+++ src/main/java/com/traazu/auth_service/domain/dtos/auth/LogInRequest.java
@@ -2,9 +2,24 @@ package com.traazu.auth_service.domain.dtos.auth;
 
 import com.traazu.auth_service.domain.enums.UserRole;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 public record LogInRequest(
+
+    @NotBlank(message = "The ipAddress has not been defined.")
+    String ipAddress,
+
+    @NotBlank(message = "The username has not been defined.")
     String username,
+
+    @NotBlank(message = "The password has not been defined.")
     String password,
+
+    @NotNull(message = "The role has not been defined.")
     UserRole role,
+
+    @NotNull(message = "The deviceInfo has not been defined.")
     DeviceInfo deviceInfo
+    
 ) {}
